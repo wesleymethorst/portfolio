6eefb428b970ad837139dc0be29098f2a1ef85ce
wesleymethorst <wesley.methorst@gmail.com>
2025-03-12 10:24:21 +0100

    Remove unused Tailwind CSS files and contact form implementation

--- contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    echo "Bedankt, $name! We hebben je bericht ontvangen.";
-}
-?>
